245d67aeb9036825af87b0c6f70b39672a8622d1
WilliamJGrace <williamjgrace@outlook.com>
2020-07-31 12:03:00 +0100

    G: Calculator renders a display component

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -6,7 +6,7 @@ import { shallow } from 'enzyme'
 describe('Display', () => {
     let wrapper
     
-    beforeEach(() => wrapper = shallow(<Display />));
+    beforeEach(() => wrapper = shallow(<Display displayValue={''} />));
 
     it('should render a <div />', () => {
         expect(wrapper.find('div').length).toEqual(1);
