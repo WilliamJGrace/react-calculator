c81937089b7616e2e29ae3be8e5cc96a2d3e4057
WilliamJGrace <williamjgrace@outlook.com>
2020-07-31 11:43:40 +0100

    display test file, R:should render a div

---
+++ src/components/Display/Display.spec.js
@@ -0,0 +1,14 @@
+import Display from 'Display'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+
+describe('Display', () => {
+    let wrapper
+    
+    beforeEach(() => wrapper = shallow(<Display />));
+
+    it('should render a <div />', () => {
+        expect(wrapper.find('div').length).toEqual(1);
+      });
+});
