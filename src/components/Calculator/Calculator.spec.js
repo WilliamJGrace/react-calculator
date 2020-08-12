3cbc6685248c90aa00cb6792700291123ef0d699
WilliamJGrace <williamjgrace@outlook.com>
2020-08-12 11:24:36 +0100

    R: calculator should render display and keypad

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -13,8 +13,18 @@ describe('Calculator', () => {
         expect(wrapper.find('div').length).toEqual(1);
       });
 
-    it('should render the Display Component', () => {
-        expect(wrapper.containsMatchingElement(<Display displayValue={wrapper.instance().state.displayValue}/>)).toEqual(true)
-    });
+      it('should render the Display and Keypad Components', () => {
+        expect(wrapper.containsAllMatchingElements([
+          <Display displayValue={wrapper.instance().state.displayValue} />,
+          <Keypad
+            callOperator={wrapper.instance().callOperator}
+            numbers={wrapper.instance().state.numbers}
+            operators={wrapper.instance().state.operators}
+            setOperator={wrapper.instance().setOperator}
+            updateDisplay={wrapper.instance().updateDisplay}
+          />
+        ])).toEqual(true);
+      });
+
 
 });
