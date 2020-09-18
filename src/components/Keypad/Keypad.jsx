1d63b662466cfef86f6e20fbeaf8a8dfc4a6ae65
WilliamJGrace <williamjgrace@outlook.com>
2020-09-18 17:05:34 +0100

    G: Keypad renders a key component

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Key from '../Key/Key'
 import './Keypad.css'
 
 const Keypad = ({ callOperator, numbers, operators, setOperator, updateDisplay }) => {
@@ -7,6 +8,7 @@ const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
 const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p>)
 
 return (
+  <>
 
 
 <div className="keypad-container">
@@ -17,6 +19,12 @@ return (
         {operatorKeys}
   </div>
 </div>
+ <Key
+ keyAction={callOperator}
+ keyType=""
+ keyValue=""
+/>
+</>
 
 
 )}
