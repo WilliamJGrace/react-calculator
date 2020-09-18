3abe593e85f1881afbe68544d79eefedfc39913b
WilliamJGrace <williamjgrace@outlook.com>
2020-09-18 16:51:41 +0100

    R: Key should render a div

---
+++ src/components/Key/Key.spec.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import Key from '../Key/Key'
+
+
+describe('Key', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<Key 
+            keyAction={jest.fn()}
+            keyType={''}
+            keyValue={''}
+            />
+            )
+    })
+
+
+    it('should render a div', () => {
+        expect(wrapper.find('div')).toEqual(1)
+        
+    });
+    
+});
